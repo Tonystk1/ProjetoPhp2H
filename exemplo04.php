c306e1ab9e7114bc7990fbe85e0659254464e805
Tony.stk1 <antoniomarcosbs1@outlook.com>
2024-03-11 19:48:11 -0300

    exemplo 04 exemplo de como concatenar

--- exemplo04.php
+++ exemplo04.php
@@ -5,7 +5,9 @@
 
 		$soma = $valor1 + $valor2;
 
-		echo "A soma de $valor1 mais $valor2 é $soma";
-		
+		echo "A soma de $valor1 mais $valor2 é $soma <br / >";
+
+		echo 'A soma de '. $valor1 . ' mais ' . $valor2 . ' é ' . $soma . '<br />';
+
 
  ?>
